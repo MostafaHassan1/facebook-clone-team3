75366682ec745a60bdcdf62116b9800ee6dda14e
ibrahim ahmed <hema.1998.man@gmail.com>
2020-09-26 21:24:36 +0200

    Clean Dirty Code 2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Validate_signup;
@@ -7,68 +9,67 @@ use App\Http\Requests\Validate_Login;
 use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str ;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','signin','verifyUser']]);
+        $this->middleware('auth:api', ['except' => ['login', 'signin', 'verifyUser']]);
     }
 
     public function signin(Validate_signup $request)
     {
         $validated = $request->validated();
         $new_code = Str::random(50);
-        if($validated == true )
-        {
-            User::create(array_merge($validated,['vcode'=>$new_code]) ) ;
+        if ($validated == true) {
+            User::create(array_merge($validated, ['vcode' => $new_code]));
 
             /******************Mail-Block******************/
-            $email= $request->email;
+            $email = $request->email;
             $name = $request['firstname'];
             $subject  = 'Verify Mail To Login';
 
-            Mail::send('email.verify', ['name' => $request->firstname, 'verification_code' => $new_code],
-            function($mail) use ($email, $name, $subject){
-                $mail->from('test@gmail.com');
-                $mail->to($email, $name);
-                $mail->subject($subject);
-            });
-             /******************Mail-Block******************/
-            return response()->json(['success'=>"Check your email inbox for verification link"],200);
-        }}
+            Mail::send(
+                'email.verify',
+                ['name' => $request->firstname, 'verification_code' => $new_code],
+                function ($mail) use ($email, $name, $subject) {
+                    $mail->from('test@gmail.com');
+                    $mail->to($email, $name);
+                    $mail->subject($subject);
+                }
+            );
+            /******************Mail-Block******************/
+            return response()->json(['success' => "Check your email inbox for verification link"], 200);
+        }
+    }
 
     public function login(Validate_Login $request)
     {
         $validated = $request->validated();
-        $credentials = $request->only('email','password');
-        $user = DB::table('users')->where('email',$request->email)->first();
-        $check = $user->email_verified_at ;
-        //$test = Auth::user()
-        if ($token = $this->guard()->attempt($credentials))
-        {
-            if(is_null($check))
-           {
-            return response()->json(['error'=>"Please check your email inbox for verfication email"],401);
-           }
-           return $this->respondWithToken($token);
-        }
-        else
-        {
-            return response()->json(['error'=>"Wrong credintials, Please try to login with a valid e-mail and password"],401);
+        if ($validated == true) {
+            $credentials = $request->only('email', 'password');
+            if ($token = $this->guard()->attempt($credentials)) {
+                $user = auth()->user()->email_verified_at;
+                if (is_null($user)) {
+                    return response()->json(['error' => "Please check your email inbox for verfication email"], 401);
+                }
+                return $this->respondWithToken($token);
+            } else {
+                return response()->json(['error' => "Wrong credintials, Please try to login with a valid e-mail and password"], 401);
+            }
         }
     }
 
     public function verifyUser($verification_code)
     {
-       $check = DB::table('users')->where('vcode',$verification_code)->first();
-        if(!is_null($check))
-        {
-            DB::table('users')->where('id', $check->id) ->update(['email_verified_at' => now() ]);
-            return response()->json([ 'success'=> true, 'message'=> 'successfully verified email address.'],200);
+        $check = DB::table('users')->where('vcode', $verification_code)->first();
+        if (!is_null($check)) {
+            DB::table('users')->where('id', $check->id)->update(['email_verified_at' => now()]);
+            //return response()->json(['success'=> true,'message'=>'successfully verified email address.'],200);
+            return view('welcome');
         }
-       return response()->json(['success'=> false, 'error'=> "Verification code is invalid."],401);
+        return response()->json(['success' => false, 'error' => "Verification code is invalid."], 401);
     }
 
     public function me()
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Mail\MailtrapExample;
 use Illuminate\Support\Facades\Mail;
 
-Route::get('/send-mail', function () {
-
-    Mail::to('newuser@example.com')->send(new MailtrapExample());
-
-    return 'A message has been sent to Mailtrap!';
-
-});
 
 Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');
 
 Route::get('/', function () {
     return view('welcome');
 });
-// test only // sry new test //
